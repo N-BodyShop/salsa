92e35a4eb9da65882c9373b30f0756b57cd7ddbb
Graeme Lufkin <gwl@u.washington.edu>
2003-08-12 21:15:02 +0000

    Removed logarithmic and reversed color scale options.

--- ResolutionServer/run_server.sh
+++ ResolutionServer/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #./charmrun ++server ++server-port 1235 $* ./ResolutionServer ../ParallelGravity/scaling/lambs.00200_subsamp_10000.pos ../ParallelGravity/scaling/lambs.00200_subsamp_10000.potential -v -v -v -r -l
-./charmrun ++server ++server-port 1235 $* ./ResolutionServer -l -r -v -v -v simlist
+./charmrun ++server ++server-port 1235 $* ./ResolutionServer -v -v -v -v simlist
