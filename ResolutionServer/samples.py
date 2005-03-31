5697e9eca7b7567a5339e9c46a209229a96b42fc
Tom Quinn <trq@astro.washington.edu>
2005-03-31 09:10:28 +0000

    Added some more examples.

--- ResolutionServer/samples.py
+++ ResolutionServer/samples.py
@@ -1,3 +1,12 @@
 # Sum of masses:
 
 ck.printclient(str(charm.getAttributeSum('All', 'mass'))+'\n')
+
+# "setsphere"
+charm.createGroupAttributeSphere("test2", "All", "position", 0, 0, 0, .1)
+
+# "setbox"
+charm.createGroupAttributeBox("test3", "All", "position", 0, 0, 0, .1, 0, 0, 0, .1, 0, 0, 0, .1)
+
+# Some info
+ck.printclient(str(charm.getCenterOfMass("test2")))
