fa8c384aa34f0aa67dbdf2b8cdf27ba4e72452d8
Tom Quinn <trq@astro.washington.edu>
2009-08-14 18:56:25 +0000

    Point to X86_64 jogl libraries if needed.

--- InteractiveClient/run.sh
+++ InteractiveClient/run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #  Run just the Salsa client
-export LD_LIBRARY_PATH=`pwd`":$LD_LIBRARY_PATH"
+machine=`uname -m`
+if test $machine = "x86_64" ; then
+   ld_path=jogl-1.1.1-linux-amd64/lib
+fi
+export LD_LIBRARY_PATH=$ld_path:$LD_LIBRARY_PATH
 java -Xmx512m -jar Salsa.jar localhost 1234 $@
