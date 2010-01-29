7238d3e1e5d96e1db82b1c73b89109cbe7876d46
James Gray <grayje@astro.washington.edu>
2010-01-29 06:07:49 +0000

    Initial version of centralized constants system. Follows Python convention.

---
+++ ResolutionServer/tipsy/config.py
@@ -0,0 +1,23 @@
+import traceback, math
+
+# constants
+msolunit  = 1.e12          # mass scale
+kpcunit   = 1.             # distance scale
+fhydrogen = 0.76           # fraction of gas as hydrogen 
+KPCCM     = 3.085678e21    # kiloparsec in centimeters
+GCGS      = 6.67e-8        # G in cgs
+MSOLG     = 1.99e33        # solar mass in grams
+GYRSEC    = 3.155693e16    # gigayear in seconds
+MHYDR     = 1.67e-24       # mass of hydrogen atom in grams
+KBOLTZ    = 1.381e-16      # bolzman constant in cgs
+GAMMA     = (5.0/3.0)      # gamma of ideal gas
+SIGMAES   = 6.665e-25      # Thomson cross section
+ME        = 9.11e-28       # electron mass in grams
+C         = 2.998e10       # speed of light in cm/sec
+
+# derived constants
+time_unit = math.sqrt(pow(kpcunit*KPCCM, 3.) / (GCGS*msolunit*MSOLG)) / GYRSEC
+
+
+
+
