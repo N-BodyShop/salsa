0fe8595d135246872226235c0701337cbcb7ccd9
James Gray <grayje@astro.washington.edu>
2010-04-13 03:37:41 +0000

    Renamed start.py to tipsy.py to be consistent with import method.

---
+++ ResolutionServer/tipsy/tipsy.py
@@ -0,0 +1,12 @@
+import charm
+
+charm.loadSimulation('run99.std')
+print 'Simulation run99.std was loaded.'
+
+from tprofile import profile
+from tshortfns import boxstat
+from tshortfns import setsphere
+from tshortfns import setbox
+from tshortfns import markgal
+from tshortfns import markbox
+
