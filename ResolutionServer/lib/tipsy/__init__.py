04e0245721c061cc3da793a3917687c6cf2de2df
Tom Quinn <trq@astro.washington.edu>
2010-09-07 15:41:37 -0700

    Moved tipsy.py to __init__.py to facilitate loading of tipsy module.

---
+++ ResolutionServer/lib/tipsy/__init__.py
@@ -0,0 +1,16 @@
+# The main import module for the Python implementation of TIPSY.
+#
+# Performing "import tipsy" will place the TIPSY commands in the
+# Python symbol table under "tipsy" so that they can be called
+# as "tipsy.function()" for example "tipsy.profile(...)"
+
+from tprofile import profile
+from trot_cur import rotcur
+from trot_cur import rotationcurve
+from tshortfns import boxstat
+from tshortfns import setsphere
+from tshortfns import setbox
+from tshortfns import markgal
+from tshortfns import markbox
+from tshortfns import writemark
+
